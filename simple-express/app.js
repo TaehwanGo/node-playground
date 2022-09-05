4006d5c93e2ab6c58dc7e5be523bc581f56dcd68
Tony <gth1123@naver.com>
2022-09-05 22:15:10 +0900

    간단히 로그 확인

--- simple-express/app.js
+++ simple-express/app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.post("/api/v1/endpoint", (req, res) => {
+app.post("/api/v1/upload", (req, res) => {
   console.log("res", res);
+  // console.log("res.body", res.body); // formData는 res.body에서 확인이 안됨
   res.send("Hello World!");
+});
+
+app.post("/api/v1/test", (req, res) => {
+  console.log("res.body", res.body);
+  res.send("Hello World! /api/v1/test");
+});
+
+app.get("/api/v1/test", (req, res) => {
+  res.send("Hello World! /api/v1/test");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World! /");
 });
 
 app.listen(port, () => {
