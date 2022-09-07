7f83167d3731cee814411fb14364afd61201b3e1
TaehwanGo <gth1123@bubblecon.io>
2022-09-07 10:52:01 +0900

    upload, memo

--- simple-express/app.js
+++ simple-express/app.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const cors = require("cors");
+
+const PORT = 4000;
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.post("/api/v1/upload", (req, res) => {
   console.log("res", res);
+  console.log("gotcha");
   // console.log("res.body", res.body); // formData는 res.body에서 확인이 안됨
   res.send("Hello World!");
+});
+
+app.post("/api/v1/memo", (req, res) => {
+  console.log("res.body", res.body);
+  console.log("memo");
+  res.send("memo!");
+});
+
+app.get("/api/v1/memo", (req, res) => {
+  console.log("get memo");
+  res.json("It is a memo");
 });
 
 app.post("/api/v1/test", (req, res) => {
@@ -21,6 +42,6 @@ app.get("/", (req, res) => {
   res.send("Hello World! /");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
