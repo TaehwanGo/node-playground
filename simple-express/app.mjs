b8f192b45d4dd95d31f7853db22dd701f67dacef
TaehwanGo <gth1123@bubblecon.io>
2022-09-19 16:55:41 +0900

    비밀번호 확인 api추가

--- simple-express/app.mjs
+++ simple-express/app.mjs
@@ -11,6 +11,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // 웹 관련 API
 
@@ -63,29 +65,32 @@ app.get("/api/v1/trainings", (req, res) => {
           total: randomNum,
         };
       }),
-    // [
-    //   {
-    //     id: "aaa",
-    //     name: "충남교육을 위한 새로운 도약 2022 교육전문직원 미래교육과정 특별연수",
-    //     location: "1층 대강당",
-    //     startedAt: "2021-01-01",
-    //     endedAt: "2021-01-02",
-    //     total: "100",
-    //   },
-    //   {
-    //     id: "bbb",
-    //     name: "유아 행복교육 실현 위한 유아교육 정책 인성 교육 직무연수",
-    //     location: "3층 행복나눔터",
-    //     startedAt: "2021-02-01",
-    //     endedAt: "2021-02-02",
-    //     total: "10",
-    //   },
-    // ],
     meta: {
       page: 1,
       isLastPage: false,
     },
   });
+});
+
+app.post("/api/v1/check-qr-enter-password", (req, res) => {
+  const { password, id } = req.body;
+  console.log("password", password);
+  console.log("id", id);
+  if (password === "1234") {
+    res.json({
+      data: {
+        isCorrect: true,
+        message: "비밀번호가 일치합니다.",
+      },
+    });
+  } else {
+    res.json({
+      data: {
+        isCorrect: false,
+        message: "비밀번호가 일치하지 않습니다.",
+      },
+    });
+  }
 });
 
 // test
